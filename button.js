ab5f555a7ea665e09e965ba37a04f57d091976ce
Lihuen <lihudiaz@gmail.com>
2023-02-28 19:45:13 -0300

    Proyecto HTML

---
+++ button.js
@@ -0,0 +1,24 @@
+document.getElementById ('button').onclick = function (){
+    console.log("Capturamos el evento click")
+    onclick=document.getElementById("organizacion").innerHTML = "Persona ordenada, comprometida y que sabe cumplir con tiempos de entrega. No solo organizado en mis tiempos, sino también en mi espacio, documentos y herramientas de trabajo."
+}
+
+document.getElementById ('button2').onclick = function (){
+    console.log("Capturamos el evento click")
+    onclick=document.getElementById("adaptabilidad").innerHTML = "Me desenvuelvo fácilmente en cualquier lugar o situación, suelo congeniar e integrarme de forma sencilla no solo en un equipo de trabajo, sino con otros departamentos que requieran de mi apoyo."
+}
+
+document.getElementById ('button3').onclick = function (){
+    console.log("Capturamos el evento click")
+    onclick=document.getElementById("integridad").innerHTML = "Me considero realista, franco y recíproco en mis relaciones. Aplico el dicho 'no hagas lo que no te gustaría que te hicieran'."
+}
+
+document.getElementById ('button4').onclick = function (){
+    console.log("Capturamos el evento click")
+    onclick=document.getElementById("resolucion-de-problemas").innerHTML = "Soy una persona activa y con iniciativa, no temo buscar varias alternativas para encontrar la solución a un problema y no dejo de buscarla hasta encontrarla."
+}
+
+document.getElementById ('button5').onclick = function (){
+    console.log("Capturamos el evento click")
+    onclick=document.getElementById("competencias-digitales").innerHTML = "Puedo manejar con sencillez las tecnologías actuales básicas y un poco más. Manejo con habilidad y facilidad una computadora, un smartphone, tablet u otros dispositivos tecnológicos, me gusta conocer la funcionabilidad de las aplicaciones y herramientas digitales que utilizo."
+}
